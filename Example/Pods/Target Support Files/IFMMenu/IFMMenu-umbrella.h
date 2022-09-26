364e9f3bf7c8bc7e6d9d23d10f2f5c844d84241e
张茨一飞 <zhangys2007@163.com>
2022-09-26 13:21:45 +0800

    张茨一飞
    新加pod库

---
+++ Example/Pods/Target Support Files/IFMMenu/IFMMenu-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "IFMMenu.h"
+#import "IFMMenuContainerView.h"
+#import "IFMMenuItem.h"
+#import "IFMMenuView.h"
+
+FOUNDATION_EXPORT double IFMMenuVersionNumber;
+FOUNDATION_EXPORT const unsigned char IFMMenuVersionString[];
+
